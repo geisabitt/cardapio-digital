38e2d095bfd0ce331857f09c479b7a146cb31fcd
geisabitt <ageisislane@gmail.com>
2024-11-26 11:00:50 -0300

    ajustando endpoints

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,9 +5,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
-export const getBurgers = () => api.get<SnackData[]>('/burgers')
-export const getPizzas = () => api.get<SnackData[]>('/pizzas')
-export const getDrinks = () => api.get<SnackData[]>('/drinks')
-export const getIceCreams = () => api.get<SnackData[]>('/ice-creams')
+export const getBurgers = () => api.get<SnackData[]>('/snacks?snack=burger')
+export const getPizzas = () => api.get<SnackData[]>('/snacks?snack=pizza')
+export const getDrinks = () => api.get<SnackData[]>('/snacks?snack=drink')
+export const getIceCreams = () => api.get<SnackData[]>('/snacks?snack=ice-cream')
 
 export default api
